d6397d8968bd10783cc899b4c2df6886b46c3d4c
Connor Wiseman <wiseman.connor@gmail.com>
2016-01-19 11:05:22 -0700

    KeyHandler + comments
    Completed KeyHandler and added documentation.

--- g.js
+++ g.js
@@ -1,6 +1,8 @@
 'use strict';
 
-
+/**
+ * @author Connor Wiseman
+ */
 (function(exports) {
 
 
@@ -8,20 +10,30 @@
         var gameCanvas = document.createElement('canvas');
 
 
+        // Acquire a 2D drawing context.
         CanvasHandler.drawSurface = gameCanvas.getContext('2d');
 
 
+        /**
+         * Sets the dimensions of the game's canvas.
+         * @argument {Number} width - A value to set as the width.
+         * @argument {Number} height - A value to set as the height.
+         */
         CanvasHandler.setDimensions = function(width, height) {
             gameCanvas.setAttribute('width', width);
             gameCanvas.setAttribute('height', height);
         };
 
 
+        /**
+         * Clears the game's canvas.
+         */
         CanvasHandler.clear = function() {
             this.drawSurface.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
         }
 
 
+        // Wait for the document to fully load, then append the game canvas.
         document.addEventListener('DOMContentLoaded', function() {
             document.body.appendChild(gameCanvas);
         });
@@ -29,12 +41,17 @@
 
 
     (function(KeyHandler) {
+        /**
+         * An object to be populated with pressed keys.
+         * @namespace
+         */
         var pressedKeys = {};
 
 
+        // A map of keys to listen for.
         var keyMap = {
             16: 'SHIFT', 17: 'CONTROL', 18: 'ALT', 32: 'SPACE',
-            37: 'LEFT', 38: 'UP', 39: 'RIGHT', 39: 'DOWN',
+            37: 'LEFT', 38: 'UP', 39: 'RIGHT', 40: 'DOWN',
 
             48: '0', 49: '1', 50: '2', 51: '3', 52: '4',
             53: '5', 54: '6', 55: '7', 56: '8', 57: '9',
@@ -48,16 +65,36 @@
         };
 
 
+        /**
+         * Checks the current state of a specified key name.
+         * @argument {String} keyName - The name of the key to check.
+         * @returns {Boolean} Whether or not the specified key is pressed.
+         */
         KeyHandler.isPressed = function(keyName) {
-
+            if (pressedKeys.hasOwnProperty(keyName)) {
+                return pressedKeys[keyName];
+            }
+            return false;
         };
 
+
+        /**
+         * Gets the name of a key associated with a specified keyboard event.
+         * @argument {KeyboardEvent} e - A keyboard event.
+         * @return {String} The name of the key.
+         */
         var getKeyName = function(e) {
             e = e || window.event;
-            var key = (which in e) ? e.which : e.keyCode;
+            var key = (typeof e.which !== 'undefined') ? e.which : e.keyCode;
             return keyMap[key];
         };
 
+
+        /**
+         * Retrieves the key name from a specified keyboard event and adds it
+         * to the list of pressed keys.
+         * @argument {KeyboardEvent} e - A keyboard event.
+         */
         var pressKey = function(e) {
             if (e.defaultPrevented) {
                 return;
@@ -67,17 +104,43 @@
             var keyName = getKeyName(e);
             if (!pressedKeys.hasOwnProperty(keyName)) {
                 pressedKeys[keyName] = true;
-                console.log(pressedKeys);
             }
         };
 
+
+        /**
+         * Retrieves the key name from a specified keyboard event and removes
+         * it from the list of pressed keys.
+         * @argument {KeyboardEvent} e - A keyboard event.
+         */
         var releaseKey = function(e) {
-            console.log(e);
+            if (e.defaultPrevented) {
+                return;
+            }
+
+
+            var keyName = getKeyName(e);
+            if (pressedKeys.hasOwnProperty(keyName)) {
+                delete pressedKeys[keyName];
+            }
         };
 
+
+        /**
+         * Sets the specified key name in the list of pressed keys to false.
+         * Used to manually interrupt a key press.
+         * @argument {String} keyName - The name of a key to interrupt.
+         */
+        KeyHandler.interrupt = function(keyName) {
+            if (pressedKeys.hasOwnProperty(keyName) && pressedKeys[keyName]) {
+                pressedKeys[keyName] = false;
+            }
+        };
+
+
+        // Listen for key presses and releases.
         document.addEventListener('keydown', pressKey);
         document.addEventListener('keyup', releaseKey);
-        
     })(exports.Key = {});
 
 
