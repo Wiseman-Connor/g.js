3273f530e9194928bdf51388514a992ff4a46bcb
Connor Wiseman <wiseman.connor@gmail.com>
2016-01-19 18:28:33 -0700

    drawRectangle, onKeyPress
    Added drawRectangle functionality to g.Canvas
    Changed behavior of onKeyPress event attachment function

--- g.js
+++ g.js
@@ -30,6 +30,55 @@
 
 
         /**
+         * Converts a hexadecimal color string to an RGB color string.
+         * @argument {String} hexValue - A hexadecimal color value.
+         * @returns {String} The hexValue in rgb(r, g, b) format.
+         * @author Tim Down
+         * @url http://stackoverflow.com/a/5624139
+         * @private
+         */
+        var hexToRGB = function(hexValue) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValue);
+            if (result) {
+                var r = parseInt(result[1], 16),
+                    g = parseInt(result[2], 16),
+                    b = parseInt(result[3], 16);
+                return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+            }
+
+
+            throw new SyntaxError('Argument 1 of drawRectangle must be a valid hexadecimal color string.');
+        };
+
+
+        /**
+         * Draws a rectangle of the specified color, at the specified coordinates,
+         * of the specified size, and with the specified alpha value.
+         * @argument {String} color  - The color of the rectangle.
+         * @argument {Number} x      - The x position of the rectangle.
+         * @argument {Number} y      - The y position of the rectangle.
+         * @argument {Number} width  - The width of the rectangle.
+         * @argument {Number} height - The height of the rectangle.
+         * @argument {Number} alpha  - The alpha value of the rectangle.
+         */
+        CanvasHandler.drawRectangle = function(color, x, y, width, height, alpha) {
+            var alphaCheck = (typeof alpha !== undefined && (alpha >= 0 || alpha <= 1));
+            if (alphaCheck) {
+                this.drawSurface.globalAlpha = alpha;
+            }
+
+
+            this.drawSurface.fillStyle = hexToRGB(color);
+            this.drawSurface.fillRect(x, y, width, height);
+
+
+            if (alphaCheck) {
+                this.drawSurface.globalAlpha = 1.0;
+            }
+        };
+
+
+        /**
          * Clears the game's canvas.
          */
         CanvasHandler.clear = function() {
@@ -195,7 +244,7 @@
 
         /**
          * @class
-         * @classdesc A new GameObject.
+         * @classdesc A generic game object container.
          * @argument {String} name - The name of the object.
          * @private
          */
@@ -204,11 +253,20 @@
                 enumerable: true,
                 value: name
             });
+
+
+            Object.defineProperty(this, 'keyEvents', {
+                configurable: true,
+                value: {}
+            });
         };
 
 
         /**
-         * @private
+         * Transposes a object of properties in key-value form onto a
+         * specified GameObject.
+         * @argument {Object} properties - A list of objects to transpose.
+         * @returns {GameObject} this
          */
         Object.defineProperty(GameObject.prototype, 'setProperties', {
             enumerable: true,
@@ -222,10 +280,21 @@
 
 
         /**
+         * Executes all attached key events, then executes any manually defined
+         * update logic. Run automatically by requestAnimationFrame.
          * @private
          */
         Object.defineProperty(GameObject.prototype, '__updateFunction', {
             value: function() {
+                // Execute all attached key events.
+                for (var keyEvent in this.keyEvents) {
+                    if (exports.Key.isPressed(keyEvent)) {
+                        this.keyEvents[keyEvent].apply(this);
+                    }
+                }
+
+
+                // Execute any manually defined update logic.
                 if (this.hasOwnProperty('update') && typeof this.update === 'function') {
                     this.update();
                 }
@@ -234,6 +303,8 @@
 
 
         /**
+         * Executes any manually defined redraw logic. Run automatically by
+         * requestAnimationFrame.
          * @private
          */
         Object.defineProperty(GameObject.prototype, '__redrawFunction', {
@@ -252,10 +323,10 @@
 
 
         /**
-         * An inline alias for g.Key.isPressed.
+         * Attaches a callback function to a GameObject that listens for the
+         * specified key.
          * @argument {String}    keyName - The name of a key.
          * @argument {KeyState} callback - A callback function.
-         * @returns {Boolean} Whether the specified key is pressed.
          */
         Object.defineProperty(GameObject.prototype, 'onKeyPress', {
             enumerable: true,
@@ -268,9 +339,7 @@
                 }
 
 
-                if (exports.Key.isPressed(keyName)) {
-                    callback.apply(this);
-                }
+                this.keyEvents[keyName] = callback;
             }
         });
 
